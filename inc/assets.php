2a8c6d7f1c01764a0ac33aa99260e00c474267ff
Kai Thoresen <kaithoresen@gmail.com>
2026-08-11 11:19:04 +0200

    feat: add lightweight responsive theme script

--- inc/assets.php
+++ inc/assets.php
@@ -6,4 +6,5 @@ defined( 'ABSPATH' ) || exit;
 add_action( 'wp_enqueue_scripts', function () {
 	$version = wp_get_theme()->get( 'Version' );
 	wp_enqueue_style( 'fyfaen-style', get_stylesheet_uri(), array(), $version );
+	wp_enqueue_script( 'fyfaen-theme', get_template_directory_uri() . '/assets/js/theme.js', array(), $version, true );
 } );
