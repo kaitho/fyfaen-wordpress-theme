a573e62435b23a886a8eff2f3cfd2f35b484558b
Kai Thoresen <kaithoresen@gmail.com>
2026-08-11 13:40:15 +0200

    chore: keep lightweight theme script

--- inc/assets.php
+++ inc/assets.php
@@ -4,7 +4,7 @@
 defined( 'ABSPATH' ) || exit;
 
 add_action( 'wp_enqueue_scripts', function () {
-	$version = wp_get_theme()->get( 'Version' );
-	wp_enqueue_style( 'fyfaen-style', get_stylesheet_uri(), array(), $version );
-	wp_enqueue_script( 'fyfaen-theme', get_template_directory_uri() . '/assets/js/theme.js', array(), $version, true );
+    $version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_style( 'fyfaen-style', get_stylesheet_uri(), array(), $version );
+    wp_enqueue_script( 'fyfaen-theme', get_template_directory_uri() . '/assets/js/theme.js', array(), $version, true );
 } );
