1fdc16e6d48df99414357dd16a7b2f7254f45e00
Kai Thoresen <kaithoresen@gmail.com>
2026-08-11 13:46:05 +0200

    feat: load dedicated product styling

--- inc/assets.php
+++ inc/assets.php
@@ -7,4 +7,8 @@ add_action( 'wp_enqueue_scripts', function () {
     $version = wp_get_theme()->get( 'Version' );
     wp_enqueue_style( 'fyfaen-style', get_stylesheet_uri(), array(), $version );
     wp_enqueue_script( 'fyfaen-theme', get_template_directory_uri() . '/assets/js/theme.js', array(), $version, true );
+
+    if ( function_exists( 'is_product' ) && is_product() ) {
+        wp_enqueue_style( 'fyfaen-product', get_template_directory_uri() . '/assets/css/product.css', array( 'fyfaen-style' ), $version );
+    }
 } );
