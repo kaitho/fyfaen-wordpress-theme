565f6db803029359f2ab3a475d4ef581ed2d6ee5
Kai Thoresen <kaithoresen@gmail.com>
2026-08-11 11:11:20 +0200

    feat: add theme bootstrap

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * FYFAEN theme bootstrap.
+ *
+ * The theme owns presentation. WooCommerce and payment plugins remain responsible
+ * for commerce, checkout, orders and payment processing.
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+require_once get_template_directory() . '/inc/theme-setup.php';
+require_once get_template_directory() . '/inc/assets.php';
+require_once get_template_directory() . '/inc/woocommerce.php';
