7cb3a1a780ed3667a5cc21111d9450d59457e3bd
Kai Thoresen <kaithoresen@gmail.com>
2026-08-17 12:23:58 +0200

    content: refine shop intro text

--- archive-product.php
+++ archive-product.php
@@ -19,7 +19,7 @@ get_header( 'shop' );
                 <?php else : ?>
                     <h1>Hele <em>kolleksjonen.</em></h1>
                 <?php endif; ?>
-                <p class="fyfaen-shop__intro">Oversized tees, hoodies, sokker og mer. Norsk brand med fokus på kvalitet, passform og særpreg.</p>
+                <p class="fyfaen-shop__intro">Oversized tees, hoodies, sokker. Norsk brand med fokus på kvalitet, passform og særpreg.</p>
             </div>
         </header>
 
